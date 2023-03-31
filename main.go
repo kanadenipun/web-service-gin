65c84b26262e7ba78befd4cdce4202c601f6a383
kanadenipun <kanadenipun@gmail.com>
2023-03-31 23:39:55 +0530

    add POST album endpoint

--- main.go
+++ main.go
@@ -15,16 +15,27 @@ type album struct {
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John C", Price: 56.99},
 	{ID: "2", Title: "Red Train", Artist: "John D", Price: 56.99},
-	{ID: "3", Title: "Green Train", Artist: "John E", Price: 56.99},
+	{ID: "3", Title: "Greenish Train", Artist: "John E", Price: 56.99},
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbum)
 
 	router.Run("localhost:18000")
 }
